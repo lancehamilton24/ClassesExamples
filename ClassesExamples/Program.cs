e4012dd9c96a543bbcd3343368a0aa90768959ca
Lance Hamilton <lance.hamilton24@gmail.com>
2019-03-05 17:42:02 -0600

    added lego heads and torso

--- ClassesExamples/Program.cs
+++ ClassesExamples/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using ClassesExamples.Legos;
+using ClassesExamples.Legos.Heads;
+using ClassesExamples.Legos.Torsos;
 
 namespace ClassesExamples
 {
@@ -6,7 +9,29 @@ namespace ClassesExamples
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var taffy = new Candy("Orange", "Orange Saltwater Taffy", CandyType.Stretchy);
+
+            taffy.SetRating(10);
+            taffy.SetRating(5, "Just kinda liked it.");
+
+            Console.WriteLine(taffy.ToString());
+
+            var myDogHead = new Dog();
+
+            myDogHead.Talk();
+
+            var martinHead = new Bald();
+            var astronaught = new Astronaut();
+
+            var fitTorso = new FitTorso(2);
+            fitTorso.Crunch(12);
+
+            var minifigure = new Minifigure(astronaught, fitTorso);
+
+            minifigure.Greet();
+
+
+            Console.ReadLine();
         }
     }
 }
